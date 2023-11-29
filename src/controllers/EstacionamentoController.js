51940893756582114f1dbad866749070674642df
Leonardo Boa Sorte <leoboasorte.dev@outlook.com>
2023-11-29 16:29:57 -0300

    GET POST E PUT CONCLUIDOS, DELETE AINDA BUGADO

---
+++ src/controllers/EstacionamentoController.js
@@ -0,0 +1,68 @@
+const { response } = require("express");
+const {v4: uuid} = require("uuid")
+const Estacionamento = require("../models/Estacionamento")
+
+module.exports = {
+    async index(request, response) {
+        try {
+            const carros = await Estacionamento.find();
+            return response.status(200).json({ carros })
+        } catch (err) {
+            response.status(500).json({ error: err.message });
+        }
+    },
+
+    async parking(request, response) {
+        const { cliente, placa, modelo } = request.body;
+
+        if(!cliente || !placa || !modelo) {
+            return response.status(400).json({error: "Faltando informações"})
+        }
+
+        const client = new Estacionamento ({
+            _id: uuid(),
+            cliente,
+            placa,
+            modelo
+        })
+
+        try {
+            await client.save();
+
+            return response.status(201).json({message:"Cliente adicionado com sucesso"})
+        } catch (err) {
+            response.status(400).json({ error: err.message });
+        }
+    },
+
+    async update(request, response) {
+        const { cliente, placa, modelo } = request.body;
+
+        if(!cliente && !placa && !modelo) {
+            return response
+                .status(400)
+                .json({ error: "Necessário informar cliente, placa ou modelo"})
+        }
+        
+        if (cliente) response.clientP.cliente = cliente;
+        if (placa) response.clientP.placa = placa;
+        if (modelo) response.clientP.modelo = modelo;
+
+        try {
+            await response.clientP.save()
+            return response.status(200).json({ message: "Atualizado com sucesso."})
+        } catch(err) {
+            response.status(500).json({ error: err.message })
+        }
+    },
+
+    async delete(request, response) {
+        try {
+            await response.clientP.remove();
+            return response.status(200).json({message:"Cliente deletado"})
+        } catch(err) {
+            return response.status(500).json({ error: err.message })
+        }
+    }
+
+}
