776b7e6001c24da7889c00b4ae1d821c185f7a65
Leonardo Boa Sorte <leoboasorte.dev@outlook.com>
2023-12-01 17:17:58 -0300

    cors adicionado

--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const express = require('express');
 const mongoose = require("mongoose")
 const routes = require('./routes')
 const connectToDatabase = require("./database")
+const cors = require("cors");
 
 connectToDatabase();
 
 const app = express ()
 const port = 3333;
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
